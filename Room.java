27791c68e5900e98f220ce8730b587d318dcd6fd
kodalishashank <sk6084@srmist.edu.in>
2026-03-03 16:06:39 +0530

    Use Case 4: Room Search & Availability Check

--- Room.java
+++ Room.java
@@ -16,3 +16,15 @@ public abstract class Room {
         System.out.println("Price per night: " + pricePerNight);
     }
 }
+
+class SingleRoom extends Room {
+    public SingleRoom() { super(1, 250, 1500.0); }
+}
+
+class DoubleRoom extends Room {
+    public DoubleRoom() { super(2, 400, 2500.0); }
+}
+
+class SuiteRoom extends Room {
+    public SuiteRoom() { super(3, 750, 5000.0); }
+}
